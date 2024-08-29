f95ebeb0787e172f374e5971bd938cc624b08d57
ChoiKangSub <cks4718@gmail.com>
2024-08-29 20:23:32 +0900

    되는건가...?

---
+++ src/main/java/com/cinema/chart/chartDAO.java
@@ -0,0 +1,15 @@
+package com.cinema.chart;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+ 
+@Mapper
+public interface chartDAO {
+
+ArrayList<chartDTO> chartList();
+ArrayList<chartDTO> chartList1();
+String chartList2(int id);
+ArrayList<chartDTO> chartList3(int id);
+}
+
